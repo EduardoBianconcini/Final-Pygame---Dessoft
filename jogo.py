3e563cfd0cc20059135a665da81e8d8c58773778
Eduardo Bianconcini <eduardobbs@al.insper.edu.br>
2024-05-13 18:36:31 -0300

    acertos

--- jogo.py
+++ jogo.py
@@ -1,14 +1,15 @@
-#importa as bibliotecas necessarias
-import pygame 
-import os 
+import pygame
+import os
+import random
 
+# Inicializa o Pygame
 pygame.init()
 
-#Define o tamanho da tela
+# Define o tamanho da tela
 altura = 600
 largura = 1000
 
-#Define as cores
+# Define as cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -16,44 +17,27 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-#Define uma variavel que indica os frames por segundo
+# Define uma variável que indica os frames por segundo
 tempo = pygame.time.Clock()
 
-#define a tela
-tela = pygame.display.set_mode((largura,altura))
+# Define a tela
+tela = pygame.display.set_mode((largura, altura))
 
-#Define O nome que vai aparecer na tela
+# Define o nome que vai aparecer na tela
 pygame.display.set_caption('Junque Jumper')
 
-#Define as imagens
-# image = pygame.image.load('assets/img/logo-madfox.png').convert()
-
-CORRIDA =  [pygame.image.load(os.path.join('Dino','DinoRun1.png')),
-            pygame.image.load(os.path.join('Dino','DinoRun2.png'))]
-
-PULO =     pygame.image.load(os.path.join('Dino','DinoJump.png'))
-
-AGACHAR =  [pygame.image.load(os.path.join('Dino','DinoDuck1.png')),
-            pygame.image.load(os.path.join('Dino','DinoDuck2.png'))]
-
-Cacto_G =  [pygame.image.load(os.path.join('cacto','LargeCactus1.png')),
-            pygame.image.load(os.path.join('cacto','LargeCactus2.png')),
-            pygame.image.load(os.path.join('cacto','LargeCactus3.png'))]
-
-Cacto_P =  [pygame.image.load(os.path.join('cacto','SmallCactus1.png')),
-            pygame.image.load(os.path.join('cacto','SmallCactus2.png')),
-            pygame.image.load(os.path.join('cacto','SmallCactus3.png'))]
-
-Ptero =     [pygame.image.load(os.path.join('ptero','Bird1.png')),
-            pygame.image.load(os.path.join('ptero','Bird2.png'))]
-
-Nuvem =     pygame.image.load(os.path.join('outro','Cloud.png'))
-
-Chao =      pygame.image.load(os.path.join('outro','chão.png'))
-
-End_game =  pygame.image.load(os.path.join('outro','Cloud.png'))
-
-Reset =     pygame.image.load(os.path.join('outro','Reset.png'))
-
-
-
+# Define as imagens
+CORRIDA = [pygame.image.load(os.path.join('Dino', 'DinoRun1.png')),
+           pygame.image.load(os.path.join('Dino', 'DinoRun2.png'))]
+PULO = pygame.image.load(os.path.join('Dino', 'DinoJump.png'))
+AGACHAR = [pygame.image.load(os.path.join('Dino', 'DinoDuck1.png')),
+           pygame.image.load(os.path.join('Dino', 'DinoDuck2.png'))]
+Cacto_G = [pygame.image.load(os.path.join('cacto', 'LargeCactus1.png')),
+           pygame.image.load(os.path.join('cacto', 'LargeCactus2.png')),
+           pygame.image.load(os.path.join('cacto', 'LargeCactus3.png'))]
+Cacto_P = [pygame.image.load(os.path.join('cacto', 'SmallCactus1.png')),
+           pygame.image.load(os.path.join('cacto', 'SmallCactus2.png')),
+           pygame.image.load(os.path.join('cacto', 'SmallCactus3.png'))]
+Ptero = [pygame.image.load(os.path.join('ptero', 'Bird1.png')),
+         pygame.image.load(os.path.join('ptero', 'Bird2.png'))]
+Chao = pygame.image.load(os.path.join('outro', 'chão.png'))
