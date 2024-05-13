21739ba602e66af272e4e2c1dc477b72b30b4599
Eduardo Bianconcini <eduardobbs@al.insper.edu.br>
2024-05-13 17:06:05 -0300

    commit

---
+++ Pygame.py
@@ -0,0 +1,56 @@
+#importa as bibliotecas necessarias
+import pygame 
+import os 
+
+pygame.init()
+
+#Define o tamanho da tela
+altura = 600
+largura = 1000
+
+#Define as cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+#Define uma variavel que indica os frames por segundo
+tempo = pygame.time.Clock()
+
+#define a tela
+tela = pygame.display.set_mode((largura,altura))
+
+#Define O nome que vai aparecer na tela
+pygame.display.set_caption('Dinosaur')
+
+#Define as imagens
+# image = pygame.image.load('assets/img/logo-madfox.png').convert()
+
+CORRIDA =  [pygame.image.load(os.path.join('Dino','DinoRun1.png')),
+            pygame.image.load(os.path.join('Dino','DinoRun2.png'))]
+
+PULO =     pygame.image.load(os.path.join('Dino','DinoJump.png'))
+
+AGACHAR =  [pygame.image.load(os.path.join('Dino','DinoDuck1.png')),
+            pygame.image.load(os.path.join('Dino','DinoDuck2.png'))]
+
+Cacto_G =  [pygame.image.load(os.path.join('cacto','LargeCactus1.png')),
+            pygame.image.load(os.path.join('cacto','LargeCactus2.png')),
+            pygame.image.load(os.path.join('cacto','LargeCactus3.png'))]
+
+Cacto_P =  [pygame.image.load(os.path.join('cacto','SmallCactus1.png')),
+            pygame.image.load(os.path.join('cacto','SmallCactus2.png')),
+            pygame.image.load(os.path.join('cacto','SmallCactus3.png'))]
+
+Ptero =     [pygame.image.load(os.path.join('ptero','Bird1.png')),
+            pygame.image.load(os.path.join('ptero','Bird2.png'))]
+
+Nuvem =     pygame.image.load(os.path.join('outro','Cloud.png'))
+
+Chao =      pygame.image.load(os.path.join('outro','chão.png'))
+
+End_game =  pygame.image.load(os.path.join('outro','Cloud.png'))
+
+Reset =     pygame.image.load(os.path.join('outro','Reset.png'))
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Final-Pygame---Dessoft
 
-1. Membros: Eduardo Bianconcini, Pedro Monteiro, Lucas Briones
+1. Título: Junque Jumper (JJ)
 
-2. Jogo: 
+2. Membros: Eduardo Bianconcini, Pedro Monteiro, Lucas Briones
+
+3. Jogo: Jogo do Dinossauro
+
+4. Vídeo Curso
+
